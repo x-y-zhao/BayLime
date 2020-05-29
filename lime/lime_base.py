0f1dabaebd23bfb30e833827c8b98c8cc7970df7
Xingyu.Zhao <47721473+weidade3721@users.noreply.github.com>
2020-05-29 11:57:53 +0100

    more results

--- lime/lime_base.py
+++ lime/lime_base.py
@@ -207,7 +207,7 @@ class LimeBase(object):
             print('using Bay_info_prior option for model regressor')
             model_regressor=BayesianRidge_inf_prior(fit_intercept=True,
                                          n_iter=0, tol=0.001,  
-                                         alpha_init=0.01, lambda_init=0.01)
+                                         alpha_init=10, lambda_init=10)
         #XZ: we set the alpha_init and lambda_init to play with different priors
         #XZ: TODO read those parameters from config files
             
